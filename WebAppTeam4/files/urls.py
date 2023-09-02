b1c72a0cb9e9a67d02a3e6a333899e17ebe46593
Anton Petrenko <114150459+Petrenko-Anton@users.noreply.github.com>
2023-09-02 20:43:40 +0300

    Delete WebAppTeam4 directory
    Signed-off-by: Anton Petrenko <114150459+Petrenko-Anton@users.noreply.github.com>

--- WebAppTeam4/files/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'files'
-
-urlpatterns = [
-    path('/files/', views.index, name='main'),  # files:main
-    ]
--- WebAppTeam4/news/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'news'
-
-urlpatterns = [
-    path('/news/', views.index, name='main'),
-    ]
