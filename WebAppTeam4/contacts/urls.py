f2e67e2842735e6432fefac7a19d875661e244d3
Anton Petrenko <114150459+petrenko-anton@users.noreply.github.com>
2023-09-02 21:13:14 +0300

    Merge pull request #4 from Petrenko-Anton/add-contact-form
    Add contact form

--- WebAppTeam4/contacts/urls.py
+++ WebAppTeam4/contacts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-#from .views import contact_page_view, add_contact_page_view, contact_list_page_view, contactbook_page_view
+from .views import contact_page_view, add_contact_page_view, contact_list_page_view, contactbook_page_view
 app_name = 'contacts'
 
 urlpatterns = [
