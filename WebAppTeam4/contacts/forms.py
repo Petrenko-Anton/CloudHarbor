b007230381e8ad14d6f2c24e8b20a4ae2d3bd40e
alimak86 <alisaakusheva15@gmail.com>
2023-09-06 22:48:01 -0400

    draft#4
    изменила вывод дат на украинский

--- WebAppTeam4/contacts/forms.py
+++ WebAppTeam4/contacts/forms.py
@@ -3,6 +3,21 @@ from django.forms import ModelForm, CharField, TextInput, EmailField, EmailInput
 
 from .models import Contact
 
+MONTHS = {
+    1: "січня",
+    2: "лютий",
+    3: "березень",
+    4: "квітень",
+    5: "травень",
+    6: "червень",
+    7: "липень",
+    8: "серпень",
+    9: "вересень",
+    10: "жовтень",
+    11: "листопад",
+    12: "грудень",
+}
+
 
 class ContactForm(ModelForm):
 
@@ -16,7 +31,8 @@ class ContactForm(ModelForm):
         attrs={'class': "form-control"}))
     # birth_date = DateField(required=True, widget=DateInput(
     #     attrs={'class': "form-control"}))
-    birth_date = DateField(required=True, widget=SelectDateWidget)
+    birth_date = DateField(
+        required=True, widget=SelectDateWidget(months=MONTHS))
 
 ########################################################################################
 #    comment = CharField(max_length=100, widget=TextInput(attrs={'class': "form-control"}))
