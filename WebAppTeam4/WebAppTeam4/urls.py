418baaca5b420fa8222a8d99614b35a5bcc481c6
Oleksandr Malieiev <maleev1820@gmail.com>
2023-09-10 12:10:05 +0200

    Fixed

--- WebAppTeam4/WebAppTeam4/urls.py
+++ WebAppTeam4/WebAppTeam4/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', include('contacts.urls')),
@@ -25,3 +28,5 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- WebAppTeam4/files/forms.py
+++ WebAppTeam4/files/forms.py
@@ -6,8 +6,7 @@ from .models import File
 class FileForm(ModelForm):
     description = CharField(max_length=255, widget=TextInput(attrs={"class": "form-control"}))
     path = FileField(widget=FileInput(attrs={"class": "form-control"}))
-    category = CharField(max_length=255, widget=TextInput(attrs={"class": "form-control"}))
 
     class Meta:
         model = File
-        fields = ['description', 'path', 'category']
+        fields = ['description', 'path']
