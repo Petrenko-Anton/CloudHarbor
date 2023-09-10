8915f1efa0a06defa8f1763e8ba439b7019958ef
alimak86 <alisaakusheva15@gmail.com>
2023-09-10 13:39:02 -0400

    old-version

--- WebAppTeam4/contacts/urls.py
+++ WebAppTeam4/contacts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import add_contact, index, contacts, editcontact, deletecontact, detailcontact, contact_search, birthlist
+
+from .views import add_contact, index, contacts, editcontact, deletecontact, detailcontact, birthlist, contact_search
 
 app_name = 'contacts'
 
@@ -11,8 +12,6 @@ urlpatterns = [
     path('new-contact', add_contact.as_view(), name='addcontact'),
     path('detail/<int:contact_id>', detailcontact.as_view(), name="detailcontact"),
     path('edit/<int:contact_id>', editcontact.as_view(), name='editcontact'),
-    #     path("contact_list/", contact_list.as_view(),
-    #          name="contact_list"),  # for debugging
     path('delete/<int:contact_id>', deletecontact.as_view(), name='deletecontact'),
     path('contact_search', contact_search.as_view(), name="contact_search"),
 ]
