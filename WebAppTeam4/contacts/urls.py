a92b775c83efdac963191a722a676b58a063e0bb
alimak86 <alisaakusheva15@gmail.com>
2023-09-11 01:47:02 -0400

    draft#8
    выполнено:
    вивід контактів-іменинників (через неделю)
    редагування нотатку
    пошук нотатка за title
    
    для отладки был изменнен файл notes.html (необходио добавить аккуратную иконку) и добавлен файл note_search_by_name.html и editnote.html

--- WebAppTeam4/contacts/urls.py
+++ WebAppTeam4/contacts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import add_contact, index, contacts, edit_contact, delete_contact, detailcontact, contact_search_by_name, \
-    contact_search_by_email, birthlist, contact_search
+    contact_search_by_email, birthlist
 
 app_name = 'contacts'
 
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', index.as_view(), name='main'),  # contacts:main
     path('contacts', contacts.as_view(), name='contacts'),
     path('birth-list', birthlist.as_view(), name='birthlist'),
-    path('new-contact', add_contact.as_view(),name='addcontact'),
+    path('new-contact', add_contact.as_view(), name='addcontact'),
     path('detail/<int:contact_id>', detailcontact.as_view(), name="detailcontact"),
     path('edit/<int:contact_id>', edit_contact.as_view(), name='editcontact'),
     path('delete/<int:contact_id>', delete_contact.as_view(), name='deletecontact'),
@@ -19,6 +19,4 @@ urlpatterns = [
          name="contact_search_by_name"),
     path('contact_search_by_email', contact_search_by_email.as_view(),
          name="contact_search_by_email"),
-
-    path('contact_search', contact_search.as_view(), name="contact_search"),
 ]
--- WebAppTeam4/notes/views.py
+++ WebAppTeam4/notes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import TagForm, NoteForm
+from .forms import TagForm, NoteForm, SearchNoteForm
 from .models import Tag, Note
 from django.contrib.auth.decorators import login_required
 
@@ -68,3 +68,58 @@ def set_done(request, note_id):
 def delete_note(request, note_id):
     Note.objects.get(pk=note_id, user=request.user).delete()
     return redirect(to="notes:main")
+
+
+@login_required
+def edit_note(request, note_id):
+    tags = Tag.objects.filter(user=request.user).all()
+    if request.method == "POST":
+        form = NoteForm(request.POST)
+        note = Note.objects.get(pk=note_id)
+        if form.is_valid():
+            updated_note = form.save(commit=False)
+            updated_note.user = request.user
+
+            choice_tags = Tag.objects.filter(
+                name__in=request.POST.getlist("tags"), user=request.user
+            )
+
+            for tag in note.tags.iterator():
+                note.tags.remove(tag)
+
+            for tag in choice_tags.iterator():
+                note.tags.add(tag)
+
+            Note.objects.filter(pk=note_id).update(name=updated_note.name,
+                                                   description=updated_note.description,
+                                                   )
+
+            return redirect(to="notes:main")
+        else:
+            return render(request, "notes/editnote.html", {"note": note, "form": form})
+
+    note = Note.objects.get(pk=note_id)
+    default_settings = {
+        "name": note.name,
+        "description": note.description,
+    }
+    form = NoteForm(initial=default_settings)
+    return render(request, "notes/editnote.html", {"tags": tags, "form": form, "note": note})
+
+
+def search_by_name(request):
+    if request.method == "GET":
+        form = SearchNoteForm(request.GET)
+        if form.is_valid():
+            search_name = form.cleaned_data["search_name"]
+            notes = (
+                Note.objects.filter(user=request.user, name=search_name).all()
+                if request.user.is_authenticated
+                else []
+            )
+            return render(request, "notes/notes.html", {"notes": notes})
+
+
+def note_search_page(request):
+    form = SearchNoteForm()
+    return render(request, "notes/note_search_by_name.html", {"form": form})
